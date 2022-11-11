31dcec605b9ce6d53a9f0f3fcc79ba52fedc1c7c
Gabriel V Oliveira <gabrielviol00@hotmail.com>
2022-11-11 20:38:37 -0300

    create methods get and post

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,21 @@
 import express from 'express';
 
 const app = express();
+ /**
+  * GET => Busca
+  * POST => Salvar
+  * PUT => Alterar
+  * DELETE => Deletar
+  * PATCH => Alteração especifica
+  */
 
+app.get("/", (request, response) => {
+    return response.send("Hello World")
+});
+
+app.post("/", (request, response) => {
+    return response.json({message: "Os dados foram salvos!"});
+})
 
 
 app.listen(3333, () => console.log("Server is running!"));
