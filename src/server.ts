380f75335e506737dc4e74027192f79474529b8c
Gabriel V Oliveira <gabrielviol00@hotmail.com>
2022-11-12 16:50:38 -0300

    routes for users

--- src/server.ts
+++ src/server.ts
@@ -1,23 +1,19 @@
 import 'reflect-metadata'
 import express from 'express';
 import "./database"
+import { router } from './routes';
+
+/**
+ * GET => Busca
+ * POST => Salvar
+ * PUT => Alterar
+ * DELETE => Deletar
+ * PATCH => Alteração especifica
+ */
 
 const app = express();
- /**
-  * GET => Busca
-  * POST => Salvar
-  * PUT => Alterar
-  * DELETE => Deletar
-  * PATCH => Alteração especifica
-  */
 
-app.get("/", (request, response) => {
-    return response.send("Hello World")
-});
-
-app.post("/", (request, response) => {
-    return response.json({message: "Os dados foram salvos!"});
-})
-
+app.use(express.json());
+app.use(router)
 
 app.listen(3333, () => console.log("Server is running!"));
