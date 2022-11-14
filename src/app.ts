3e7083f4f65302397e76dab397cc91184985e641
Gabriel V Oliveira <gabrielviol00@hotmail.com>
2022-11-14 12:40:48 -0300

    create: unitary tests with jest

---
+++ src/app.ts
@@ -0,0 +1,20 @@
+import 'reflect-metadata'
+import express from 'express';
+import createConnection from "./database"
+import { router } from './routes';
+
+/**
+ * GET => Busca
+ * POST => Salvar
+ * PUT => Alterar
+ * DELETE => Deletar
+ * PATCH => Alteração especifica
+ */
+
+createConnection();
+const app = express();
+
+app.use(express.json());
+app.use(router)
+
+export { app }
