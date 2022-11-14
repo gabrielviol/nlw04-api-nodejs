f9c117ef3c661d9ef5163db12683d83e8a398e71
Gabriel V Oliveira <gabrielviol00@hotmail.com>
2022-11-14 11:57:24 -0300

    create: Controller for searchs

--- src/routes.ts
+++ src/routes.ts
@@ -1,10 +1,13 @@
 import { Router } from 'express'
+import { SurveysController } from './controllers/SurveysController';
 import { UserController } from './controllers/UserController';
 
 const router = Router();
 
 const userController = new UserController();
+const SurveyController = new SurveysController();
 
 router.post("/users", userController.create);
+router.post("/surveys", SurveyController.create)
 
 export { router };
