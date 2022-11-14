2607bbcaac53fdb8301ac5674c5b07d7b93ca9ba
Gabriel V Oliveira <gabrielviol00@hotmail.com>
2022-11-14 11:21:44 -0300

    fix: business rules

--- src/database/migrations/1668433935826-CreateUsers.ts
+++ src/database/migrations/1668433935826-CreateUsers.ts
@@ -1,6 +1,6 @@
 import {MigrationInterface, QueryRunner, Table} from "typeorm";
 
-export class CreateUsers1668279520076 implements MigrationInterface {
+export class CreateUsers1668433935826 implements MigrationInterface {
 
     public async up(queryRunner: QueryRunner): Promise<void> {
         await queryRunner.createTable(
@@ -8,30 +8,30 @@ export class CreateUsers1668279520076 implements MigrationInterface {
                 name: "users",
                 columns: [
                     {
-                        name: 'id',
-                        type: 'uuid',
+                        name: "id",
+                        type: "uuid",
                         isPrimary: true
                     },
                     {
-                        name: 'name',
-                        type: 'varchar'
+                        name: "name",
+                        type: "varchar",
                     },
                     {
-                        name: 'email',
-                        type: 'varchar'
+                        name: "email",
+                        type: "varchar",
                     },
                     {
-                        name: 'created_at',
-                        type: 'timestamp',
-                        default: 'now()',
-                    }
-                ]
+                        name: "created_at",
+                        type: "timestamp",
+                        default: "now()",
+                    },
+                ],
             })
-        )
+        );
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.dropTable('users');
+        await queryRunner.dropTable("users");
     }
 
 }
