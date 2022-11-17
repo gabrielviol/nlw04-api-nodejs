2695e7d5e7561c106d88abfff3fa424ce940cb47
Gabriel V Oliveira <gabrielviol00@hotmail.com>
2022-11-16 21:51:05 -0300

    create controller for validation

--- src/routes.ts
+++ src/routes.ts
@@ -1,4 +1,5 @@
 import { Router } from 'express'
+import { AnswerController } from './controllers/AnswerController';
 import { SendMailController } from './controllers/SendMailController';
 import { SurveysController } from './controllers/SurveysController';
 import { UserController } from './controllers/UserController';
@@ -10,11 +11,15 @@ const SurveyController = new SurveysController();
 
 const sendMailController = new SendMailController();
 
+const answerController = new AnswerController();
+
 router.post("/users", userController.create);
 
 router.post("/surveys", SurveyController.create);
 router.get("/surveys", SurveyController.show);
 
 router.post("/sendMail", sendMailController.execute)
+
+router.get("/answers/:value", answerController.execute);
 
 export { router };
